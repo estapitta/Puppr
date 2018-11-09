547ccb27686768a61a51c48588a3ce31fcf5f155
Esta <efspitta@gmail.com>
2018-11-08 20:56:13 -0600

    puppy page design completed

--- screens/PuppyPage.js
+++ screens/PuppyPage.js
@@ -1,6 +1,13 @@
 import React from "react";
 
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TouchableHighlight
+} from "react-native";
+import puppyImages from "../resources/puppyImages";
 
 export default class PuppyPage extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -23,8 +30,117 @@ export default class PuppyPage extends React.Component {
     const puppy = this.props.navigation.getParam("puppy") || {};
 
     return (
-      <View>
-        <Text>{puppy.description}</Text>
+      <View style={{ flex: 1, backgroundColor: "white" }}>
+        <Image
+          style={{ width: "100%", height: "40%" }}
+          source={puppyImages[puppy.name]}
+          resizeMode="cover"
+        />
+
+        <ScrollView
+          style={{ flex: 1 }}
+          contentContainerStyle={{
+            width: "100%",
+            padding: 20
+          }}
+        >
+          <Text>{puppy.description}</Text>
+
+          <View style={{ marginTop: 15 }}>
+            <Text
+              style={{
+                flex: 1,
+                marginTop: 10
+              }}
+            >
+              <Text style={{ fontSize: 18, fontWeight: "bold" }}>Breed:</Text>
+              {"   "}
+              {puppy.breed}
+            </Text>
+            <View
+              style={{
+                width: "90%",
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                padding: 5
+              }}
+            />
+
+            <Text
+              style={{
+                flex: 1,
+                marginTop: 10
+              }}
+            >
+              <Text style={{ fontSize: 18, fontWeight: "bold" }}>Age:</Text>
+              {"   "}
+              {puppy.age}
+            </Text>
+            <View
+              style={{
+                width: "90%",
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                padding: 5
+              }}
+            />
+
+            <Text
+              style={{
+                flex: 1,
+                marginTop: 10
+              }}
+            >
+              <Text style={{ fontSize: 18, fontWeight: "bold" }}>Gender:</Text>
+              {"   "}
+              {puppy.gender}
+            </Text>
+            <View
+              style={{
+                width: "90%",
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                padding: 5
+              }}
+            />
+
+            <Text
+              style={{
+                flex: 1,
+                marginTop: 10
+              }}
+            >
+              <Text style={{ fontSize: 18, fontWeight: "bold" }}>Color:</Text>
+              {"   "}
+              {puppy.color}
+            </Text>
+            <View
+              style={{
+                width: "90%",
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                padding: 5
+              }}
+            />
+          </View>
+          <TouchableHighlight
+            style={{
+              alignItems: "center",
+              width: "70%",
+              padding: 15,
+              borderColor: "black",
+              backgroundColor: "#62AB42",
+              borderWidth: 1,
+              borderRadius: 45,
+              alignSelf: "center",
+              marginTop: 20
+            }}
+          >
+            <Text style={{ fontWeight: "bold", color: "white", fontSize: 18 }}>
+              Woof!
+            </Text>
+          </TouchableHighlight>
+        </ScrollView>
       </View>
     );
   }
